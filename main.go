095903b7cd2bc32e8d970906bcd7bb901c71cd6d
kaium123 <41822927+kaium123@users.noreply.github.com>
2022-07-28 15:50:59 +0600

    main file

--- main.go
+++ main.go
@@ -1,17 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
+	"github.com/kaium123/practice/router"
 )
-xyz
-type ServerType bool
-
-func (m ServerType) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "hello")
-}
 
 func main() {
-	var k ServerType
-	http.ListenAndServe("localhost:8000", k)
+	router.Route()
 }
